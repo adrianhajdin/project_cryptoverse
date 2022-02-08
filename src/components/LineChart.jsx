9e71756523f6567421f4e98c6e24583d2b7fe305
Md Ishaque <iamishaque.business@gmail.com>
2022-02-09 00:48:22 +0600

    v3
    1.In latest "chart.js": "^3.7.0" and "react-chartjs-2": "^4.0.1" you have to register element.
    2. Using "unshift" instead of "push" allows you to sort dates in the correct order.
    3. Multiplying the timestamp by 1000 will give the correct date.

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,36 +1,55 @@
+import { Col, Row, Typography } from 'antd'
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+)
+const { Title: AntTitle } = Typography
 
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Col, Row, Typography } from 'antd';
+const LineChart = ({ coinName, currentPrice, coinHistory }) => {
+  const coinPrice = []
+  const coinTimeStamp = []
 
-const { Title } = Typography;
-
-const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-  const coinPrice = [];
-  const coinTimestamp = [];
-
-  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinPrice.push(coinHistory?.data?.history[i].price);
+  for (let i = 0; i < coinHistory?.data?.history?.length; ++i) {
+    coinPrice.unshift(coinHistory?.data?.history[i].price)
+    coinTimeStamp.unshift(
+      new Date(
+        coinHistory?.data?.history[i].timestamp * 1000,
+      ).toLocaleDateString(),
+    )
   }
 
-  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
-  }
   const data = {
-    labels: coinTimestamp,
+    labels: coinTimeStamp,
     datasets: [
       {
-        label: 'Price In USD',
+        label: 'Price to USD',
         data: coinPrice,
         fill: false,
         backgroundColor: '#0071bd',
         borderColor: '#0071bd',
       },
     ],
-  };
+  }
 
   const options = {
-    scales: {
+    Scale: {
       yAxes: [
         {
           ticks: {
@@ -39,20 +58,26 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
         },
       ],
     },
-  };
+  }
 
   return (
     <>
       <Row className="chart-header">
-        <Title level={2} className="chart-title">{coinName} Price Chart </Title>
+        <AntTitle className="chart-title" level={2}>
+          {coinName} Price Chart
+        </AntTitle>
         <Col className="price-container">
-          <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
-          <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
+          <AntTitle className="price-change" level={5}>
+            {coinHistory?.data?.change}%
+          </AntTitle>
+          <AntTitle className="current-price" level={5}>
+            Current {coinName} Price $ {currentPrice}
+          </AntTitle>
         </Col>
       </Row>
       <Line data={data} options={options} />
     </>
-  );
-};
+  )
+}
 
 export default LineChart;
